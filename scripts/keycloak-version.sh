73e9bee724ac6c19d2f41f24f3d6bff51af878fa
Antti Leppä <antti.leppa@metatavu.fi>
2018-09-12 16:58:05 +0300

    Review changes

--- scripts/keycloak-version.sh
+++ scripts/keycloak-version.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -i final | cut -d '/' -f1 | tail -n1`
-export VERSION="4.2.1.Final"
+export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -i final | cut -d '/' -f1 | tail -n1`
 export KEYCLOAK="keycloak-${VERSION}"
